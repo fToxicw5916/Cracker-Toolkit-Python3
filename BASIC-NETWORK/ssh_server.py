f40bd0eedc1d78394fa172905d61d70544f2b701
fToxicw <ftoxicw@163.com>
2022-06-30 13:04:08 +0800

    Update ssh_server.py

--- BASIC-NETWORK/ssh_server.py
+++ BASIC-NETWORK/ssh_server.py
@@ -1,22 +1,21 @@
-#!/usr/bin/env python
-
 '''
 A SSH server.
-
-A program that creates an SSH server for our SSH client (where we'll run commands) to connect to. This could be a Linux, Windows, or even macOS system that has Python and Paramiko installed.
 '''
 # Import needed packages
 import os
-import paramiko
+import paramiko  # INSTALL BEFORE USE!
 import socket
 import sys
 import threading
 
 CWD = os.path.dirname(os.path.realpath(__file__))
-HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, '.test_rsa.key')) # We're using the SSH key in the Paramiko demo files
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, '.test_rsa.key'))  # We're using the SSH key in the Paramiko demo files
 
-# The server
+
 class Server (paramiko.ServerInterface):
+    '''
+    Server class.
+    '''
     def _init_(self):
         self.event = threading.Event()
 
@@ -29,10 +28,10 @@ class Server (paramiko.ServerInterface):
         if (username == 'tim') and (password == 'sekret'):
             return paramiko.AUTH_SUCCESSFUL
 
-# Run the program
+
 if __name__ == '__main__':
-    server = '192.168.1.207'
-    ssh_port = 2222
+    server = '127.0.0.1'
+    ssh_port = 12345
     try:
         # Create socket object
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,7 +46,7 @@ if __name__ == '__main__':
     else:
         print(f'[+] Got a connection! from {addr}')
 
-    bhSession = paramiko.Transport(client) # Configure the authencation methods
+    bhSession = paramiko.Transport(client)  # Configure the authencation methods
     bhSession.add_server_key(HOSTKEY)
     server = Server()
     bhSession.start_server(server=server)
@@ -58,7 +57,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     print('[+] Authenticated!')
-    print(chan.recv(1024).decode()) # Receive data
+    print(chan.recv(1024).decode())  # Receive data
     chan.send('Welcome to bh_ssh')
     try:
         while True:
@@ -73,4 +72,4 @@ if __name__ == '__main__':
                 bhSession.close()
                 break
     except KeyboardInterrupt:
-        bhSession.close()
+        bhSession.close()  # Close session
