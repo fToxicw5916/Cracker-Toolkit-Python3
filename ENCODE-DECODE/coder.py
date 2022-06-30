1e2765f494798c8a760b9dc9c76c5205b69ab351
fToxicw <ftoxicw@163.com>
2022-06-30 14:17:42 +0800

    Update coder.py

--- ENCODE-DECODE/coder.py
+++ ENCODE-DECODE/coder.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 '''
 A simple Base64 encoder and decoder made from Python.
 '''
@@ -11,31 +8,37 @@ en_de = input('Encode or decode? ')
 en_de = en_de.lower()
 data = input('Data: ')
 
-# Encode
+
 def encode(data):
+    '''
+    Encode
+    '''
     data2 = str.encode(data.strip())
     endata = base64.b64encode(data2)
     endata = str(endata)
     endata = endata.strip('b\'')
     print(endata)
 
-# Decode
+
 def decode(data):
+    '''
+    Decode
+    '''
     data2 = str.encode(data.strip())
     dedata = base64.b64decode(data2)
     dedata = str(dedata)
     dedata = dedata.strip('b\'')
     print(dedata)
 
-# Main function
+
 def main():
-    if en_de == 'encode': # Encode
+    if en_de == 'encode':  # Encode
         # Encode
         encode(data)
-    else: # Decode
+    else:  # Decode
         # Decode
         decode(data)
 
-# Run the program
+
 if __name__ == '__main__':
     main()
