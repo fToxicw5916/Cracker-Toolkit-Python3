b028c6308434524676fccfc8484114f33b621d3f
fToxicw <ftoxicw@163.com>
2022-06-30 14:02:35 +0800

    Update detector.py

--- ARP/detector.py
+++ ARP/detector.py
@@ -6,15 +6,15 @@ from pprint import pprint
 import cv2
 import os
 
-# Image dir
-ROOT = ''
-# Output dir
-FACES = ''
-# Train file dir
-TRAIN = ''
+ROOT = '<Image directory here>'  # Directory for images
+FACES = '<Output directory here>'  # Output directory
+TRAIN = '<Train file directory here>'  # Directory for the training file
 
-# Detector
+
 def detect(srcdir=ROOT, tgtdir=FACES, train_dir=TRAIN):
+    '''
+    Detector.
+    '''
     for fname in os.listdir(srcdir):
         if not fname.upper().endswith('.JPG'):
             continue
@@ -26,7 +26,7 @@ def detect(srcdir=ROOT, tgtdir=FACES, train_dir=TRAIN):
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Read the training file
-        training = os.path.join(train_dir, 'haarcascade_frontalface_alt.xml')
+        training = os.path.join(train_dir, 'train.xml')
         cascade = cv2.CascadeClassifier(training)
         rects = cascade.detectMultiScale(gray, 1.3, 5)
         try:
@@ -36,11 +36,11 @@ def detect(srcdir=ROOT, tgtdir=FACES, train_dir=TRAIN):
         except AttributeError:
             print(f'No faces found in {fname}')
             continue
-            # highlight the faces in the image
+            # Highlight the faces in the image
         for x1, y1, x2, y2 in rects:
             cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
         cv2.imwrite(newname, img)
 
-# Run
+
 if __name__ == '__main__':
     detect()
