1c01a0de328b7d730df1c722535b7f57a80294c7
fToxicw <ftoxicw@163.com>
2022-06-30 14:32:41 +0800

    Update dirlister.py

--- GIT/dirlister.py
+++ GIT/dirlister.py
@@ -4,9 +4,11 @@ A simple script that returns the files in the current directory.
 # Import needed packages
 import os
 
-# Main function
-def run(**args):
-    print("[*] In dirlister module.")
-    files = os.listdir('.') # List the files
-    return str(files) # Return the result
 
+def run(**args):
+    '''
+    Main function
+    '''
+    print("[*] In dirlister module.")
+    files = os.listdir('.')  # List the files
+    return str(files)  # Return the result
