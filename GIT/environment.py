b44a6850ec2773151fe2e81ea3060bd3f0aa655e
fToxicw <ftoxicw@163.com>
2022-06-30 14:33:06 +0800

    Update environment.py

--- GIT/environment.py
+++ GIT/environment.py
@@ -4,7 +4,10 @@ A script that returns the currect environment data.
 # Import needed packages
 import os
 
-# Main function
+
 def run(**args):
+    '''
+    Main function
+    '''
     print("[*] In environment module")
     return os.environ # Return data
