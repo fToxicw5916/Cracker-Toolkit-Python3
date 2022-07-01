e23f74e6c9f128f849b21b219449e916704edbc4
fToxicw <ftoxicw@163.com>
2022-07-01 14:13:15 +0800

    Update udp_client.py

--- BASIC-NETWORK/udp_client.py
+++ BASIC-NETWORK/udp_client.py
@@ -9,13 +9,12 @@ HOST = '<Your host here>'
 PORT = <Your port here>
 
 # Create a socket object and send some data to the host
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b'AAABBBCCC', (HOST, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create object
+client.sendto(b'AAABBBCCC', (HOST, PORT))  # Connect & Send data
 
 # Receive data from the host
-data, address = client.recvfrom(4096)
-print(data.decode('utf-8'))
-print(address.decode('utf-8'))
+data, address = client.recvfrom(4096)  # Receive data
+print(data.decode('utf-8'))  # Decode data
+print(address.decode('utf-8'))  # Decode data
 
-# Close the connection
-client.close()
+client.close()  # Close the connection
