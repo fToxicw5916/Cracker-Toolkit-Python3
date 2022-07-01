dfd902cfaade0e72c25e964d9ce53bc6ed741b38
fToxicw <ftoxicw@163.com>
2022-07-01 14:12:23 +0800

    Update tcp_client.py

--- BASIC-NETWORK/tcp_client.py
+++ BASIC-NETWORK/tcp_client.py
@@ -9,13 +9,12 @@ HOST = '<Your host here>'
 PORT = <Your port here>
 
 # Create a socket object and connect to the server using HOST and PORT
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create object
+client.connect((HOST, PORT))  # Connect
 
 # Send data and receive data
 client.send(b'GET / HTTP/1.1\r\nHost: <Your host here>\r\n\r\n')  # Send data
 response = client.recv(4096)  # Receive data
 print(response.decode('utf-8'))  # Print decoded data
 
-# Close the connection
-client.close()
+client.close()  # Close the connection
